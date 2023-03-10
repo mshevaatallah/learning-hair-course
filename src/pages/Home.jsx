54679ae521474f87ad3169943ecb29368ac96d62
sxhvxa <muhammadshax07@gmail.com>
2023-03-10 09:52:12 +0700

    add some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import MainImage from "../assets/images/main-image.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="main-section-container">
+        <div className="text-main-container">
+          <h1 className="main-title">Belajar Teknik Barber bersama kami ! </h1>
+          <p className="main-subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            euismod, nisl vel tincidunt lacinia, nisl nisl aliquam nisl, eu
+          </p>
+        </div>
+        <div className="image-main-container">
+          <img src={MainImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
